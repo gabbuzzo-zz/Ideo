4231c266598916f3e79f6f6bb94179fe264c91e4
gabriele.saracino9904 <gabriele.saracino9904@gmail.com>
2021-10-01 17:07:32 +0200

    Model e post controller aggiunto a ideo.backend

---
+++ Ideo.Backend/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ideo.Backend.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FiscalCode { get; set; }
+        public string Password { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Curriculum { get; set; }
+        public List<JobApplication> JobApplications { get; set; }
+        public List<Post> Posts { get; set; }
+        public List<VideoCourse> VideoCourses { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
--- Ideo/Ideo.iOS/MediaPlayerRenderer.cs
+++ Ideo/Ideo.iOS/MediaPlayerRenderer.cs
@@ -23,10 +23,8 @@ namespace Ideo.iOS
     public class MediaPlayerRenderer : ViewRenderer<Controls.MediaPlayer, UIView>
     {
         UIView view;
-        AVFoundation.AVPlayer player;
-
-
-
+        AVFoundation.AVPlayer avp;
+        AVPlayerViewController avpvc;
         protected override void OnElementChanged(ElementChangedEventArgs<Controls.MediaPlayer> e)
         {
             base.OnElementChanged(e);
@@ -36,26 +34,37 @@ namespace Ideo.iOS
             }
             if (e.NewElement != null)
             {
+                List<string> stringDir = new List<string>();
                 view = new UIView();
+                var directories = Directory.GetCurrentDirectory()+"/Resources/teamwork1.mp4";
+                
+                
+                //foreach (var directory in directories)
+                //{
+                //    Console.WriteLine(directory);
+                //    //stringDir.Add(directory);
+                //}
+                
+                NSUrl file = NSUrl.FromString("https://www.youtube.com/watch?v=PilNQ2TkwGw");
+                var apc = NSBundle.MainBundle.GetUrlForResource("teamwork1", "mp4");
                 var path = NSBundle.MainBundle.PathForResource("teamwork1", "mp4");
-                var playerController = new AVPlayerViewController();
-                NSUrl url = NSUrl.FromString(path);
-                player = new AVPlayer(url);
-                playerController.Player = player;
-                //player.Play();
-                //view.AddSubview(playerController.View);
-                //playerController.View.Frame = view.Frame;
-                playerController.ShowsPlaybackControls = false;
-                var playerLayer = AVPlayerLayer.FromPlayer(player);
-                playerLayer.Frame = playerController.View.Frame;
-                CGRect rect = new CGRect(10, 10, 200, 200);
-                playerController.View.Frame = rect;
-                view.AddSubview(playerController.View);
-                view.ClipsToBounds = true;
-                playerController.Player.Play();
-                player.Play();
-                //this.view.Layer.AddSublayer(playerLayer);
-                SetNativeControl(view);
+                var url2 = NSBundle.MainBundle.GetUrlForResource("teamwork1", "mp4");
+                var fileS = File.Exists(path);
+                avp = new AVPlayer(NSUrl.FromString(path));
+                var coder = new NSCoder();
+                avpvc = new AVPlayerViewController();
+                avpvc.Player =new AVPlayer(NSUrl.FromString(path));
+                avpvc.ShowsPlaybackControls = false;
+                var plLayer = new AVPlayerLayer() { Player = avp };
+                plLayer.Frame = this.NativeView.Bounds;
+                var duration = avp.CurrentItem.Duration;
+                this.view.Layer.AddSublayer(plLayer);
+                //view.Add();
+                //playerLayer!.frame = self.view!.bounds
+                //self.view!.layer.addSublayer(playerLayer!)
+                avp.Play();
+                avpvc.Player.Play();
+                    SetNativeControl(avpvc.View);
 
             }
         }
